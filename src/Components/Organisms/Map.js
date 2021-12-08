9029736210b3ae1e0edaaef99559a28ccd241c6e
alexnicholls1999 <azini@live.co.uk>
2021-12-08 21:44:36 +0000

    add changes to mk5

--- src/Components/Organisms/Map.js
+++ src/Components/Organisms/Map.js
@@ -1,21 +1,17 @@
 import PropTypes from "prop-types"
 import { MapContainer, Popup, TileLayer, Marker, useMap} from 'react-leaflet'
-import useGeoloation from "../../react-hooks/useGeolocation";
-import GPS from "../Atoms/GPS"
 
 function Map({map}) {
-    const mapRef = useMap();
-    const { position } = useGeoloation(mapRef)
+
 
     return (
-        <MapContainer center={[11, -0.121212]} zoom={map.zoom} scrollWheelZoom={true}>
+        <MapContainer center={[11, -0.121212]} zoom={12} scrollWheelZoom={true}>
             <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" /> 
             <Marker position={[11, -0.121212]}>
                 <Popup>
                     Hello there! <br /> General Kenobi
                 </Popup>
             </Marker>
-            <GPS position={position}/>
         </MapContainer>
     )
 }
--- src/Pages/Navigation.js
+++ src/Pages/Navigation.js
@@ -1,13 +1,10 @@
 import Map from "../Components/Organisms/Map"
-import useGeolocation from "../react-hooks/useGeolocation"
-
 function Navigation() {
 
-    const { position } = useGeolocation();
     
     const zoom = 12
 
-    return <Map map={{position: position, zoom: zoom}}/>
+    return <Map />
 }
 
 export default Navigation
