e659b6d1afc0dca0db53c097fd172cc571d20fa1
alexnicholls1999 <azini@live.co.uk>
2022-01-16 12:42:03 +0000

    add changes to sass

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
-import './styles/global.sass';
+import './styles/styles.sass';
 
 ReactDOM.render(
   <React.StrictMode>
